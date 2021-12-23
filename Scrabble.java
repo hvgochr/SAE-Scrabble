235d73d8e1caeec8a2d5e0e092f29f70385c1569
hvgochr <95345156+hvgochr@users.noreply.github.com>
2021-12-23 19:40:19 +0100

    2nd commit

--- Scrabble.java
+++ Scrabble.java
@@ -1,7 +1,9 @@
 public class Scrabble{
 
     private static int [] nbPointsJeton = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 10, 1, 2, 1, 1, 3, 8, 1, 1, 1, 1, 4, 10, 10, 10, 10};
-    public static void main(String[]args){
-        
-    }
+    private Joueur[] joueurs;
+    private int numJoueur; // joueur courant (entre 0 et joueurs.length-1)
+    private Plateau plateau;
+    private MEE sac;
+    
 }

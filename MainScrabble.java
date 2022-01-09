1b4921e9674affb253918d1b92f9446aacfcebe5
hvgochr <95345156+hvgochr@users.noreply.github.com>
2022-01-09 18:57:54 +0100

    Update MainScrabble.java

--- MainScrabble.java
+++ MainScrabble.java
@@ -1,18 +1,20 @@
 public class MainScrabble {
 
-    public static void main (String[]args){
-        Ut.afficherSL("Veuillez entrer le nombre de joueurs: "); int nbJoueurs = Ut.saisirEntier();
-        while(nbJoueurs<2 || nbJoueurs>4){
+    public static void main(String[] args) {
+        Ut.afficherSL("Veuillez entrer le nombre de joueurs: ");
+        int nbJoueurs = Ut.saisirEntier();
+        while (nbJoueurs < 2 || nbJoueurs > 4) {
             Ut.afficherSL("Vous devez saisir un nombre de joueurs compris entre 2 et 4.");
-            Ut.afficherSL("Veuillez entrer le nombre de joueurs: "); nbJoueurs = Ut.saisirEntier();
+            Ut.afficherSL("Veuillez entrer le nombre de joueurs: ");
+            nbJoueurs = Ut.saisirEntier();
         }
-        String[]tabJoueurs = new String[nbJoueurs];
-        for(int i=0; i<tabJoueurs.length; i++){
-            Ut.afficherSL("Entrez le nom du joueur N°"+(i+1)+": ");
-            tabJoueurs[i]=Ut.saisirChaine();
+        String[] tabJoueurs = new String[nbJoueurs];
+        for (int i = 0; i < tabJoueurs.length; i++) {
+            Ut.afficherSL("Entrez le nom du joueur N°" + (i + 1) + ": ");
+            tabJoueurs[i] = Ut.saisirChaine();
         }
         Scrabble scrabble = new Scrabble(tabJoueurs);
         scrabble.partie();
     }
-    
+
 }
